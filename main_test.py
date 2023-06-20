68d9b59c0af3ce48b5c43d64b9f09ca5c69f3ffa
ishaansavla <ishaan.savla16@gmail.com>
2023-06-20 10:33:29 -0700

    Fix

--- main_test.py
+++ main_test.py
@@ -21,7 +21,7 @@ def test_main_1():
     print(res.group())
 
     res = re.search(r'[\w,\W]*Python[\w,\W]*', lines[1])
-    assert res != None
+    assert res is not None
     print(res.group())
 
     res = re.search(r'[\w,\W]*Programming[\w,\W]*Python[\w,\W]*', lines[2])
