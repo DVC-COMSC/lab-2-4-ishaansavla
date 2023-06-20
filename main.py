d271980ee32a6b2cce2907d1d3d1cbaa150538a4
ishaansavla <ishaan.savla16@gmail.com>
2023-06-20 10:21:27 -0700

    Added Features

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 def main():
+    original_str = "Python Programming"
+    sub1 = original_str[0:6]
+    sub2 = original_str[7:19]
+
+    merged_str = sub2 + " " + sub1
+    print(merged_str)
     ##################################################
     # Comlete your code here
     ##################################################
